8cb5a5c422bf09db9bdc75a7b49aadd1fc61a8eb
Eric Marmier <Rico@erics-air.home>
2017-07-17 00:09:50 +0200

    test

---
+++ app/src/main/java/akumapps/android/masterb/Parameters.java
@@ -0,0 +1,14 @@
+package akumapps.android.masterb;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Parameters extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_parameters);
+
+    }
+}
